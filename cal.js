2cc252427bf1d74e1d3b790b6edbeab306f8a54c
Jhonatan <zemanatejhonatan857@gmail.com>
2025-11-18 15:21:53 -0500

    calculadora2

--- cal.js
+++ cal.js
@@ -1,5 +1,29 @@
-let pantalla = document.getElementById("pantalla");
+let pantalla = document.getElementById('pantalla');
+let valorActual = "";
 
-function enviar(num){
-    alert("Hola mundo");
+function enviarvalue(valor) {
+    valorActual += valor;
+    pantalla.value = valorActual;
+}
+
+
+function clearpantalla() {
+    valorActual = "";
+    pantalla.value = "";
+}
+
+
+function eliminar() {
+    valorActual = valorActual.slice(0, -1);
+    pantalla.value = valorActual;
+}
+
+
+function resultado() {
+    try {
+        valorActual = eval(valorActual);
+        pantalla.value = valorActual;
+    } catch (e) {
+        pantalla.value = "Error";
+    }
 }
