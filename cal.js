b5a3b2d06739956cda091272a2051314d49bfbf8
Jhonatan <zemanatejhonatan857@gmail.com>
2025-11-18 16:58:15 -0500

    arreglo calculadora

--- cal.js
+++ cal.js
@@ -1,29 +1,46 @@
 let pantalla = document.getElementById('pantalla');
-let valorActual = "";
 
-function enviarvalue(valor) {
-    valorActual += valor;
-    pantalla.value = valorActual;
-}
+let button1 = document.getElementById('1');
+button1.onclick = () => {
+    alert(button1.innerText);
+};
 
+let button2 = document.getElementById('2');
+button2.onclick = () => {
+    alert(button2.innerText);
+};
 
-function clearpantalla() {
-    valorActual = "";
-    pantalla.value = "";
-}
+let button3 = document.getElementById('3');
+button3.onclick = () => {
+    alert(button3.innerText);
+};
 
+let button4 = document.getElementById('4');
+button4.onclick = () => {
+    alert(button4.innerText);
+};
 
-function eliminar() {
-    valorActual = valorActual.slice(0, -1);
-    pantalla.value = valorActual;
-}
+let button5 = document.getElementById('5');
+button5.onclick = () => {
+    alert(button5.innerText);
+};
 
+let button6 = document.getElementById('6');
+button6.onclick = () => {
+    alert(button6.innerText);
+};
 
-function resultado() {
-    try {
-        valorActual = eval(valorActual);
-        pantalla.value = valorActual;
-    } catch (e) {
-        pantalla.value = "Error";
-    }
-}
+let button7 = document.getElementById('7');
+button7.onclick = () => {
+    alert(button7.innerText);
+};
+
+let button8 = document.getElementById('8');
+button8.onclick = () => {
+    alert(button8.innerText);
+};
+
+let button9 = document.getElementById('9');
+button9.onclick = () => {
+    alert(button9.innerText);
+};
