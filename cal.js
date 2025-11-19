f11970072ffe6a2085a3c4a5bc1d352117cc6ad1
Jhonatan <zemanatejhonatan857@gmail.com>
2025-11-19 16:49:21 -0500

    calculadora

--- cal.js
+++ cal.js
@@ -1,46 +1,34 @@
-let pantalla = document.getElementById('pantalla');
+let input = document.getElementById('pantalla');
 
-let button1 = document.getElementById('1');
-button1.onclick = () => {
-    alert(button1.innerText);
-};
+let button1 = document.getElementById('boton1');
+let button2 = document.getElementById('boton2');
+let button3 = document.getElementById('boton3');
+let button4 = document.getElementById('boton4');
+let button5 = document.getElementById('boton5');
+let button6 = document.getElementById('boton6');
+let button7 = document.getElementById('boton7');
+let button8 = document.getElementById('boton8');
+let button9 = document.getElementById('boton9');
+let button0 = document.getElementById('boton0');
 
-let button2 = document.getElementById('2');
-button2.onclick = () => {
-    alert(button2.innerText);
-};
+button1.onclick = () => { alert(button1.innerText); };
+button2.onclick = () => { alert(button2.innerText); };
+button3.onclick = () => { alert(button3.innerText); };
+button4.onclick = () => { alert(button4.innerText); };
+button5.onclick = () => { alert(button5.innerText); };
+button6.onclick = () => { alert(button6.innerText); };
+button7.onclick = () => { alert(button7.innerText); };
+button8.onclick = () => { alert(button8.innerText); };
+button9.onclick = () => { alert(button9.innerText); };
+button0.onclick = () => { alert(button0.innerText); };
 
-let button3 = document.getElementById('3');
-button3.onclick = () => {
-    alert(button3.innerText);
-};
-
-let button4 = document.getElementById('4');
-button4.onclick = () => {
-    alert(button4.innerText);
-};
-
-let button5 = document.getElementById('5');
-button5.onclick = () => {
-    alert(button5.innerText);
-};
-
-let button6 = document.getElementById('6');
-button6.onclick = () => {
-    alert(button6.innerText);
-};
-
-let button7 = document.getElementById('7');
-button7.onclick = () => {
-    alert(button7.innerText);
-};
-
-let button8 = document.getElementById('8');
-button8.onclick = () => {
-    alert(button8.innerText);
-};
-
-let button9 = document.getElementById('9');
-button9.onclick = () => {
-    alert(button9.innerText);
-};
+function enviar(num){
+    input.value += num;
+}
+function calcular() {
+  try {
+    input.value = eval(input.value);
+  } catch {
+    input.value = "Error";
+  }
+}
